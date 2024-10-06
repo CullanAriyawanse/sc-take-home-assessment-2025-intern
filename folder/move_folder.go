c27a719841388460b64c03257f0f3acb5db0676a
Cullan <z5363760@ad.unsw.edu.au>
2024-10-06 17:52:11 +1100

    feat: Implemented MoveFolder() function without any edge case checks. Added extra base case test for MoveFolder() function

--- folder/move_folder.go
+++ folder/move_folder.go
@@ -1,7 +1,54 @@
 package folder
 
-func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
-	// Your code here...
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
 
-	return []Folder{}, nil
+func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
+	folderData := make([]Folder, len(f.folders))
+	copy(folderData, f.folders)
+	// folderData := f.folders
+
+	// var err error
+
+	var parentFolderPath string
+
+	// Get path to parent folder
+	for _, folder := range folderData {
+		fmt.Printf("Folder name is %s\n", folder.Name)
+		if folder.Name == dst {
+			parentFolderPath = folder.Paths
+			break
+		}
+	}
+
+	fmt.Printf("Parent folder is %s!\n", parentFolderPath)
+
+	var newFullPath string
+	// Change path of subtree
+	for i, folder := range folderData {
+		if folder.Name == name {
+			newFullPath = parentFolderPath + "." + name
+			fmt.Printf("Old path is %s\n", folder.Paths)
+			folderData[i].Paths = newFullPath
+			fmt.Printf("New path is %s\n", folder.Paths)
+			break
+		}
+	}
+
+	// Change path of all children nodes
+	for i, folder := range folderData {
+		splitString := strings.Split(folder.Paths, ".")
+
+		containsParent := slices.Contains(splitString, name)
+
+		if containsParent && folder.Name != name && folder.Name != dst {
+			folderData[i].Paths = newFullPath + "." + folder.Name
+			break
+		}
+	}
+
+	return folderData, nil
 }
--- folder/move_folder_test.go
+++ folder/move_folder_test.go
@@ -23,25 +23,44 @@ func Test_folder_MoveFolder(t *testing.T) {
 			name:     "bravo",
 			dst:      "delta",
 			folders: []folder.Folder{
-				{Name: "alpha", OrgId: uuid.FromStringOrNil("org1"), Paths: "alpha"},
-				{Name: "bravo", OrgId: uuid.FromStringOrNil("org1"), Paths: "alpha.bravo"},
-				{Name: "charlie", OrgId: uuid.FromStringOrNil("org1"), Paths: "alpha.bravo.charlie"},
-				{Name: "delta", OrgId: uuid.FromStringOrNil("org1"), Paths: "alpha.delta"},
+				{Name: "alpha", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "alpha"},
+				{Name: "bravo", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "alpha.bravo"},
+				{Name: "charlie", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "alpha.bravo.charlie"},
+				{Name: "delta", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "alpha.delta"},
 			},
 			want: []folder.Folder{
-				{Name: "alpha", OrgId: uuid.FromStringOrNil("org1"), Paths: "alpha"},
-				{Name: "bravo", OrgId: uuid.FromStringOrNil("org1"), Paths: "alpha.delta.bravo"},
-				{Name: "charlie", OrgId: uuid.FromStringOrNil("org1"), Paths: "alpha.delta.bravo.charlie"},
-				{Name: "delta", OrgId: uuid.FromStringOrNil("org1"), Paths: "alpha.delta"},
+				{Name: "alpha", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "alpha"},
+				{Name: "bravo", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "alpha.delta.bravo"},
+				{Name: "charlie", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "alpha.delta.bravo.charlie"},
+				{Name: "delta", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "alpha.delta"},
+			},
+		},
+		{
+			testName: "Base Case 2",
+			name:     "bravo",
+			dst:      "golf",
+			folders: []folder.Folder{
+				{Name: "alpha", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "alpha"},
+				{Name: "bravo", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "alpha.bravo"},
+				{Name: "charlie", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "alpha.bravo.charlie"},
+				{Name: "delta", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "alpha.delta"},
+				{Name: "golf", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "golf"},
+			},
+			want: []folder.Folder{
+				{Name: "alpha", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "alpha"},
+				{Name: "bravo", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "golf.bravo"},
+				{Name: "charlie", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "golf.bravo.charlie"},
+				{Name: "delta", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "alpha.delta"},
+				{Name: "golf", OrgId: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), Paths: "golf"},
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.testName, func(t *testing.T) {
 			f := folder.NewDriver(tt.folders)
-			got := f.GetFoldersByOrgID(tt.orgID)
+			got, _ := f.MoveFolder(tt.name, tt.dst)
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("moveFolder() = %v, want %v", got, tt.want)
+				t.Errorf("moveFolder() = \n%v, \nwant %v", got, tt.want)
 			}
 		})
 	}
